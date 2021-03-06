a518615ab74d3dfdc0c061ff12f4bba3a7a056e0
HunteRPVP <55155885+HunteRPVP@users.noreply.github.com>
2021-03-06 16:02:22 +0300

    added back and forward button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-lecture1011';
+
+  private history: string[] = []
+
+  constructor(private router: Router, private location: Location) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.history.push(event.urlAfterRedirects)
+      }
+    })
+  }
+
+  back(): void {
+    this.history.pop()
+    if (this.history.length > 0) {
+      this.location.back()
+    } else {
+      this.router.navigateByUrl('/')
+    }
+  }
+
+  forward() {
+    this.history.pop()
+    if (this.history.length > 0) {
+      this.location.forward()
+    } else {
+      this.router.navigateByUrl('/')
+    }
+  }
 }
